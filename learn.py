5496f34eb495bded47299ecce8ff5961375a3d17
ritikrahul57-tech <ritikrahul57@gmail.com>
2026-06-25 22:25:13 +0530

    The day 1

---
+++ learn.py
@@ -0,0 +1,26 @@
+# a=7
+# b=8
+# print(a+b)
+
+
+# a=8
+# print(type(a))
+
+# b="hero"
+# print(type(b))
+
+
+#operator (+,-,5,*)
+
+# a=8
+# b=7
+# c=a+b
+# print(c)
+
+#casting
+
+
+a=int("6")
+b=int("7")
+c=a+b
+print(c)
