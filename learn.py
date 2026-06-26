beecfb9070e760e49a6b81f908eb370a7b71d09a
ritikrahul57-tech <ritikrahul57@gmail.com>
2026-06-26 22:08:10 +0530

    The python day2

--- learn.py
+++ learn.py
@@ -20,7 +20,47 @@
 #casting
 
 
-a=int("6")
-b=int("7")
-c=a+b
-print(c)
+# a=int("6")
+# b=int("7")
+# c=a+b
+# print(c)
+
+
+# a=int(input())
+# b=int(input())
+# c=a+b
+# print(c)
+
+# name=input()
+# age=input()
+# print("My name is:",name)
+# print("age is:",age)
+
+# name=input()
+# age=input()
+# address=input()
+# print("My name is:",name)
+# print("my age is:",age)
+# print("My address is:",address)
+
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# d=a*b*c
+# e=a+b+c
+# f=d/e
+# print(f)
+
+# name=input()
+# score=int(input())
+# department=input()
+# print("My name is:",name)
+# print("My score is:",score/10,"/10")
+# print("My department is:",department)
+
+
+#if and else
+
+if(true):
+    print("yess")
+else
