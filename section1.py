a456a8f1617d3aa3685955d2aa5e2b5bd4ba354f
ritikrahul57-tech <ritikrahul57@gmail.com>
2026-06-27 22:46:13 +0530

    The python day3

--- section1.py
+++ section1.py
@@ -12,3 +12,24 @@
 # else:
 #     print("Not eligible for scholarship")
 
+# number=int(input("Enter a number:"))
+# if(number%3==0 and number%5==0):
+#     print("yes it is divisible")
+# else:
+#     print("no iy cannot be divisible")
+
+
+
+# number=int(input("Enter a number"))
+# if(number%2==0):
+#     print("It is enen number")
+# else:
+#     print("It is odd number")
+
+mark=int(input("Enter your mark:"))
+if(mark<35):
+    print("Bad student")
+if(mark>35 and mark<70):
+    print("Average student")
+if(mark>70):
+    print("Good student")
