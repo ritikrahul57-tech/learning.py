1e3af2d5deaace61c8075068d635a4974f79f3f8
ritikrahul57-tech <ritikrahul57@gmail.com>
2026-06-27 22:16:52 +0530

    The python day3

---
+++ section1.py
@@ -0,0 +1,14 @@
+# #    nested and elif
+
+# mark=int(input("Enter the mark:"))
+# if (mark>35):
+#     print("Pass")
+# else:
+#     print("Fail")
+
+# income=int(input("Your income is:"))
+# if(income>7000):
+#     print("scholarship is avaible")
+# else:
+#     print("Not eligible for scholarship")
+
