981a793c95c0b48f06858f1a57171ecb4668a851
ritikrahul57-tech <ritikrahul57@gmail.com>
2026-06-28 22:55:08 +0530

    The python day 4

--- section1.py
+++ section1.py
@@ -26,12 +26,46 @@
 # else:
 #     print("It is odd number")
 
+
 # mark=int(input("Enter your mark:"))
 # if(mark<35):
 #     print("Bad student")
 # elif(mark>35 and mark<70):
 #     print("Average student")
-# if(mark>70):
+# elif(mark>70 and mark==100):
 #     print("Good student")
+# else:
+#     print("Invaild number")
 
 
+# a=int(input("Enter the first number:"))
+# b=int(input("Enter the second number:"))
+# operation=input("add/sub/mul/div:")
+# if(operation=="add"):
+    # print(a+b)
+# elif(operation=="sub"):
+    # print(a-b)
+# elif(operation=="mul"):
+    # print(a*b)
+# elif(operation=="div"):
+    # print(a/b)
+# else:
+    # print("Invalid number is given")
+
+
+
+# score=int(input("Enter your mark:"))
+# if(score>=70):
+#     name=input("Enter your name:")
+#     age=input("Enter your age:")
+#     location=input("Enter your location:")
+#     print("You are elgible")
+# else:
+#     print("You are not eligible")
+
+
+salary=int(input("Enter your salary:"))
+age=int(input("Enter your age:"))
+if(salary>=20000 or age<=25):
+    loan=input("Enter the loan amount you want:")
+    print("You are eligble for loan")
