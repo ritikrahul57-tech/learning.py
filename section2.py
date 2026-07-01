200176d48f0e1c4cee3fddf332900ed7cc5ca386
ritikrahul57-tech <ritikrahul57@gmail.com>
2026-07-01 21:53:58 +0530

    The python day6

--- section2.py
+++ section2.py
@@ -7,6 +7,20 @@
 # for i in range(1,5):
 #     print(i)
 
-for i in range(1,11):
-    print(i,"* 2 =",i*2)
+# for i in range(1,11):
+#     print(i,"* 2 =",i*2)
 
+
+#        for loop example:
+
+
+# a=int(input("Enter the first number:"))
+# b=int(input("Enter the second number:"))
+# for i in range(a+1,b):
+#     print(i)
+
+
+
+for i in range(1,11):
+    if(i%2==0):
+        print(i)
