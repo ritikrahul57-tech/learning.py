4c006a6656127662fbde5b208beac391ed732487
ritikrahul57-tech <ritikrahul57@gmail.com>
2026-07-01 23:07:53 +0530

    The python day6

--- section2.py
+++ section2.py
@@ -21,6 +21,62 @@
 
 
 
-for i in range(1,11):
-    if(i%2==0):
-        print(i)
+# for i in range(1,11):
+#     if(i%2==0):
+#         print(i)
+
+
+# count=0
+# for i in range(1,11):
+#     if(i%2==0):
+#         count=count+1
+# print(count)
+
+
+# count=0
+# o_count=0
+# for i in range(1,11):
+#     if(i%2==0):
+#         count=count+1
+#     else:
+#         o_count=o_count+1
+# print("Even:",count)
+# print("Odd:",o_count)
+
+
+# a_count=0
+# for i in range(1,101):
+#     if(i%3==0 and i%5==0):
+#         a_count=a_count+1
+# print(a_count)
+
+
+# sum=0
+# for i in range(1,6):
+#     sum=sum+i
+# print(sum)
+
+# a=[]     list[]
+# a.append(8)
+# a.append(9)
+# a.append(7)
+# b=int(input("Enter the number:"))
+# a.append(b)
+# print(a)
+
+
+a=[]
+print("Enter any 5 number:")
+for i in range(5):
+    num=int(input("Enter num:" + str(i+1)))
+    a.append(num)
+print(a)
+
+sum=0
+for i in a:
+    sum=sum+i
+print(sum)
+
+
+
+
