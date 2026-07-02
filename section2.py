47a217002d3c6038acc247ffc11f975275549069
ritikrahul57-tech <ritikrahul57@gmail.com>
2026-07-02 21:55:09 +0530

    The python day7

--- section2.py
+++ section2.py
@@ -65,17 +65,24 @@
 # print(a)
 
 
-a=[]
-print("Enter any 5 number:")
-for i in range(5):
-    num=int(input("Enter num:" + str(i+1)))
-    a.append(num)
-print(a)
+# a=[]
+# print("Enter any 5 number:")
+# for i in range(5):
+#     num=int(input("Enter num:" + str(i+1)))
+#     a.append(num)
+# print(a)
+# sum=0
+# for i in a:
+#     sum=sum+i
+# print(sum)
 
-sum=0
-for i in a:
-    sum=sum+i
-print(sum)
+
+# for i in range(1,6):
+#     print(i*i*i)
+
+
+#             Nested for loop
+
 
 
 
