1cb9090fd3755f6f93873efa4485f1f0f2d0b91b
ritikrahul57-tech <ritikrahul57@gmail.com>
2026-07-02 22:36:16 +0530

    The python day7

--- section2.py
+++ section2.py
@@ -84,6 +84,37 @@
 #             Nested for loop
 
 
+# for i in range(1,6):
+#     for j in range(1,3):
+#         print(j,"apple")
+
+
+# for i in range(1,3):
+#     print("Week:",i)
+#     for j in range(1,4):
+#         print("Day:",j)
+    
+
+# for i in range(5):
+#     print("#",end="")
+
+
+# for i in range(1,5):
+#     print()
+#     for j in range(1,i+1):
+#         print(j,end="")
+
+
+# for i in range(1,5):
+#     print("*" * i)
+
+
+
+#                while loop
+
+
+
+    
 
 
 
---
+++ section3.py
@@ -0,0 +1,12 @@
+#          while looop
+
+
+# i=0
+# while(i==0):
+#     print(i)
+#     i=i+1
+
+# i=1
+# while(i<5):
+#     print(i)
+#     i=i+1
