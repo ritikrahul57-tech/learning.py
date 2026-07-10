473ef5d411d6bef5ddf596c8c962efbd57f7c44b
ritikrahul57-tech <ritikrahul57@gmail.com>
2026-07-10 20:42:12 +0530

    The python day 14

--- section6.py
+++ section6.py
@@ -1,1 +1,41 @@
 #         classes and object with example
+
+
+# class student:
+#     def __init__(self):
+#        self.name="bloomer"
+#        self.register="456"
+#     def display(self):
+#         print("name:",self.name)
+#         print("register number:",self.register)
+# s1=student()
+# s2=student()
+# s1.name="ritik"
+# s1.register="233"
+# s2.name='rahul'
+# s2.register="444"
+# s1.display()
+# s2.display()
+
+
+# class fruit:
+#     def __init__(self,col):
+#         self.color=col
+# apple=fruit("black")
+# print(apple.color)
+
+
+# class teacher:
+#     def __init__(self,names,reg):
+#         self.name=names
+#         self.register=reg
+#     def display(self):
+#         print("Name:",self.name)
+#         print("register number:",self.register)
+# t1=teacher("Ritik","45")
+# t2=teacher("Rahul","39")
+# t1.display()
+# t2.display()
+
+
+
