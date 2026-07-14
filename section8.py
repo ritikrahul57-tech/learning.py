5e03b6da3cf37d02e64330ff368489fbb58d98cc
ritikrahul57-tech <ritikrahul57@gmail.com>
2026-07-14 22:22:10 +0530

    The python day 16

--- section8.py
+++ section8.py
@@ -1,1 +1,31 @@
-print("hopp")
+# #         super keyord in python
+
+
+# class a():
+#     def __init__(self):
+#         print("A")
+#     def display(self):
+#         print("You are in the class a")
+# class b(a):
+#     def __init__(self):
+#         super().__init__()
+#         print("B")
+#     def display():
+#         print("you are in the class b")
+# class c(b):
+#     def __init__(self):
+#         super().__init__()
+#         print("C")
+#     def display():
+#         print("you are in the class c")
+# obj=c()
+
+
+
+#           polymorphism
+
+
+
+def add():
+    print("add")
+add()
