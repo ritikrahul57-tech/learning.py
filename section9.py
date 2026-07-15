803900394247b228cfcbf992e7d7f0e5d91b551b
ritikrahul57-tech <ritikrahul57@gmail.com>
2026-07-15 22:20:00 +0530

    The python day17

--- section9.py
+++ section9.py
@@ -9,3 +9,28 @@
 #     pass        
 # b1=b()
 # print(b1._companyname)
+
+
+
+
+#          exception handling
+
+
+#   ther are three type of erro
+#       -comiple time error
+#       -logical error
+#       -run time error
+
+
+# try:
+#     a=int(input("Enter a number"))
+#     b=int(input("Enter a number"))
+#     print(a+b)
+# except Exception as e:
+#     print("something is wrong",e)
+
+
+
+
+
+
