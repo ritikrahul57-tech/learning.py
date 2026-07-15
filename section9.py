821c85d485917ce1df83ee9389fb7473b8425e00
ritikrahul57-tech <ritikrahul57@gmail.com>
2026-07-15 22:48:30 +0530

    The python programming language is done

--- section9.py
+++ section9.py
@@ -31,6 +31,54 @@
 
 
 
+# try:
+#     a=int(input())
+#     b=int(input())
+#     c=input()
+#     print(c/a)
+#     print(d)
+# except ValueError as e:
+#     print("something is wrong",e)
+# except TypeError as e:
+#     print("Somethin is go in correct",e)
+# except Exception as e:
+#     print("There are some problem in it",e)
 
 
 
+# try:
+#     a=int(input())
+#     b=int(input())
+#     print(a-b)
+# except Exception as e:
+#     print("Something is wrong",e)
+# finally:
+#     print("all are done")
+
+
+
+#       file handling
+
+
+
+# f=open("fruits.txt")
+# content=f.read()
+# print(content)
+
+
+# f=open("fruits.txt","w")
+# f.write("apple\n")
+# f.write("mango")
+# f.close
+# f=open("fruits.txt","r+")
+# print(f.read())
+
+
+
+
+# f=open("fruits.txt","a")
+# f.write("banna\n")
+# f.write("dragonfruits")
+# f.close
+# f=open("fruits.txt","r+")
+# print(f.readline())
